42965e7d6a919dfb5c8ef2b29a74b3defdb4b367
Chris Dawson <cdawson@ebay.com>
2016-12-01 15:01:32 -0800

    Write out markdown

--- slurp-images-into-markdown.go
+++ slurp-images-into-markdown.go
@@ -1,77 +1,84 @@
 package main
 
 import (
-	"fmt"
-	"path/filepath"
-	"os"
 	"flag"
-	"strconv"
-	"image/png"
+	"fmt"
 	"github.com/nfnt/resize"
+	"image/png"
+	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
+	"strconv"
 )
 
-
-func resizePng( f string, i int, size int ) {
-	// open "test.jpg"
-	file, err := os.Open( f )
+func checkErr(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+}
+
+func copy(src string, dst string) {
+	// Read all content of src to data
+	data, err := ioutil.ReadFile(src)
+	checkErr(err)
+	// Write data to dst
+	err = ioutil.WriteFile(dst, data, 0644)
+	checkErr(err)
+}
+
+func resizePng(f string, size *int, output string) {
+	// open "test.jpg"
+	file, err := os.Open(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// decode jpeg into image.Image
 	img, err := png.Decode(file)
 	if err != nil {
 		log.Fatal(err)
 	}
 	file.Close()
-	
+
 	// resize to width 1000 using Lanczos resampling
 	// and preserve aspect ratio
-	m := resize.Resize( uint(size), 0, img, resize.Lanczos3)
-	
-	output := "./" + strconv.Itoa(i) + ".resized.png"
-	fmt.Println( "Writing: ", output )
-	out, err := os.Create( output )
+	m := resize.Resize(uint(*size), 0, img, resize.Lanczos3)
+
+	fmt.Println("Writing: ", output)
+	out, err := os.Create(output)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer out.Close()
-	
+
 	// write new image to file
-	png.Encode(out, m )
+	png.Encode(out, m)
 }
 
+const formatUsage = "Filename glob, e.g.: ~/Desktop/Screen\\ Shot\\ 2016-12-01"
 
 func main() {
-	if 3 > len(os.Args)  {
-		fmt.Println( "Need to specify a directory+pattern to use and output size." )
-	} else {
-		format := flag.String( "--format", nil, "Filename glob, e.g.: ~/Desktop/Screen\ Shot\ 2016-12-01" ) // os.Args[1]
-		size := flat.Int( "--size", 800, "Resize width, defaults to 800" )
-		// size, err := strconv.Atoi( os.Args[2] )
-		if nil != err {
-			fmt.Println( "Second argument conversion error: ", err )
-		} else {
-			files, _ := filepath.Glob(format+"*")
-			if 4 == len(os.Args) {
-				if "--channels" == os.Args[3] {
-					fmt.Println( "Using channels" )
-				} else if "--goroutine" == os.Args[3] {
-					fmt.Println( "Using goroutine" )
-					for index,f := range files {
-						go resizePng( f, index, size )
-					}
-				}
-			} else {
-				files, _ := filepath.Glob(format+"*")
-				for index,f := range files {
-					resizePng( f, index, size )
-				}
-			}
-		}
-		// fmt.Println(files) // contains a list of all files in the current directory
-	}
-	fmt.Println( "Done" );
-}
+	format := flag.String("format", "", formatUsage)
+	size := flag.Int("size", 800, "Resize width")
+	convert := flag.Bool("convert", false, "Resize files")
+	flag.Parse()
 
+	if "" != *format {
+		files, _ := filepath.Glob(*format + "*")
+		for index, f := range files {
+			short := strconv.Itoa(index) + ".resized.png"
+			output := "./" + short
+			if *convert {
+				resizePng(f, size, output)
+			} else {
+				copy(f, output)
+			}
+			// Print out the files
+			fmt.Printf( "## %d\n\n[](%v)\n\n", index, short )
+		}
+	} else {
+		fmt.Println("Use -format: " + formatUsage)
+	}
+	fmt.Println("Done")
+}
